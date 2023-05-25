d3e8cd55d914cfa3b1a8a674187911f9a401725a
Fernando Flekenstein <fer.flekenstein96@gmail.com>
2023-05-25 17:40:39 -0300

    return to specific cors origin

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 app.use(cookieParser());
 app.use(cors({
-    origin: "*",
+    origin: config.app.URL_FRONT,
     credentials: true 
 }));
 initializeStrategy();
