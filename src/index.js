603fc4cbc77f4f672aecd97b4cc744685d1ff87c
Fernando Flekenstein <fer.flekenstein96@gmail.com>
2023-05-25 17:24:29 -0300

    change origin from cors

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 app.use(cookieParser());
 app.use(cors({
-    origin: config.app.URL_FRONT,
+    origin: "*",
     credentials: true 
 }));
 initializeStrategy();
